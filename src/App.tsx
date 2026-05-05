6fa7114a04c74d11844effa8fd7e91e97e41a74e
Josh <joshfromtessy@gmail.com>
2026-05-05 09:07:35 -0400

    Add Vercel Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Analytics } from "@vercel/analytics/react";
 import { Toolbar } from "./components/toolbar/Toolbar";
 import { InstructionPalette } from "./components/panels/InstructionPalette";
 import { PixiCanvas } from "./canvas/PixiCanvas";
@@ -18,6 +19,7 @@ export default function App() {
         <TagPanel />
       </div>
       <StatusBar />
+      <Analytics />
     </div>
   );
 }
